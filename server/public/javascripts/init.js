f60e1568c4d656e0c84241872f3c0de793c3d9ff
Ben Bodenmiller <bbodenmiller@hotmail.com>
2016-05-22 10:39:21 -0700

    cleanup

--- server/public/javascripts/init.js
+++ server/public/javascripts/init.js
@@ -1,14 +1,14 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15, // Creates a dropdown of 15 years to control year
-      selectTime: true
-    });
-    $('.timepicker').pickatime({
-      interval: 30
-    });
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15, // Creates a dropdown of 15 years to control year
+            selectTime: true
+        });
+        $('.timepicker').pickatime({
+            interval: 30
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
