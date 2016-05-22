1bda1a209c7fca10b4d5b973d16129b87af5c917
Lionel Lints <Lionel@Lionels-MacBook-Pro.local>
2016-05-21 21:32:42 -0700

    update

--- js/init.js
+++ js/init.js
@@ -2,7 +2,13 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year
+      selectTime: true
+    });
+    $('.timepicker').pickatime({
+      interval: 30
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
